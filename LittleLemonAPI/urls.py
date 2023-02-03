805b3e36606807facb591a04e8dde7fdf1cd4bae
Kevin Peng <fatexrebirth@gmail.com>
2023-02-03 15:31:17 +0800

    feat: User group management endpoints for delivery crew

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('groups/manager/users/', views.manager_list),
     path('groups/manager/users/<int:userId>', views.manager_detail),
+    path('groups/delivery-crew/users/', views.delivery_crew_list),
+    path('groups/delivery-crew/users/<int:userId>', views.delivery_crew_detail)
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -36,3 +36,35 @@ def manager_detail(request, userId):
         return Response({ "message": "error" }, status=status.HTTP_400_BAD_REQUEST)
     else:
         return Response({ "message": "method not allowed" }, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+@api_view(['GET', 'POST'])
+def delivery_crew_list(request):
+    if request.method == 'GET':
+        delivery_crew = []
+        for user in User.objects.all():
+            if user.groups.filter(name="Delivery Crew").exists():            
+                serializer = ManagerSerializer(user)
+                delivery_crew.append(serializer.data)
+        return Response(delivery_crew)
+    elif request.method == 'POST':
+        username = request.data['username']
+        if username:
+            user = get_object_or_404(User, username=username)
+            delivery_crew = Group.objects.get(name="Delivery Crew")
+            delivery_crew.user_set.add(user)
+            return Response({ "message": "user added to the delivery crew group" }, status=status.HTTP_201_CREATED)
+        return Response({ "message": "error" }, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({ "message": "method not allowed" }, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['DELETE'])
+def delivery_crew_detail(request, userId):
+    if request.method == 'DELETE':
+        if userId:
+            user = get_object_or_404(User, id=userId)
+            delivery_crew = Group.objects.get(name="Delivery Crew")
+            delivery_crew.user_set.remove(user)
+            return Response({ "message": "user removed from the delivery crew group" }, status=status.HTTP_200_OK)
+        return Response({ "message": "error" }, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({ "message": "method not allowed" }, status=status.HTTP_405_METHOD_NOT_ALLOWED)
