95f60b47791e0b4f50ef57cd1d59d4f3cf92e3d7
Kevin Peng <fatexrebirth@gmail.com>
2023-02-05 17:34:05 +0800

    feat: Cart model

--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, MenuItem
+from .models import Category, MenuItem, Cart
 
 admin.site.register(Category)
 admin.site.register(MenuItem)
+admin.site.register(Cart)
